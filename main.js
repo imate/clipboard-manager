fc84e7efa2505eb134ec0156664c066c2a78c6a5
Nano W User <imateee93@gmail.com>
2023-07-21 14:54:22 +0200

    First commit

---
+++ main.js
@@ -0,0 +1,83 @@
+const { app, BrowserWindow, ipcMain } = require('electron')
+const clipboardListener = require('clipboard-event');
+const clipboardy = require('clipboardy');
+const jp = require("fs-jetpack");
+
+const JSON_FILE_NAME = 'clipboard.json';
+
+let mainWindow;
+let jetpack = jp.cwd(app.getPath('userData'));
+
+const createWindow = () => {
+    mainWindow = new BrowserWindow({
+        width: 640, height: 480,
+        webPreferences: { nodeIntegration: true, contextIsolation: false }
+    })
+
+    mainWindow.loadFile('index.html');
+}
+
+app.whenReady().then(() => {
+    createWindow();
+    app.on('activate', () => { if (BrowserWindow.getAllWindows().length === 0) createWindow() });
+});
+
+app.on('window-all-closed', () => { if (process.platform !== 'darwin') app.quit() });
+
+let dataSource;// = [];
+clipboardListener.startListening();
+
+clipboardListener.on('change', () => {
+    try {
+        dataSource.unshift({ date: new Date().toISOString(), value: clipboardy.readSync() });
+        refreshList();
+    } catch (ex) {
+        console.log('Error on clipboardListener change event: ' + ex.message);
+    }
+});
+
+
+function refreshList() {
+    if (dataSource) {
+        jetpack.write(JSON_FILE_NAME, dataSource);
+    } else {
+        try {
+            dataSource = JSON.parse(jetpack.read(JSON_FILE_NAME));
+        } catch (ex) {
+            console.log(ex.message);
+            dataSource = [];
+        }
+    }
+    mainWindow.webContents.send('refresh-list', dataSource);
+}
+
+ipcMain.on('copy-item', async function (e, i) {
+    let value = dataSource[i].value
+    dataSource.splice(i, 1);
+    clipboardy.writeSync(value);
+});
+
+ipcMain.on('delete-item', async function (e, i) {
+    dataSource.splice(i, 1);
+    refreshList();
+});
+
+ipcMain.on('clear-duplicates', async function (e) {
+    let tempDS = [];
+    dataSource.forEach(element => {
+        if (!tempDS.some(x => x.value === element.value)) {
+            tempDS.push(element);
+        }
+    });
+    dataSource = tempDS;
+    refreshList();
+});
+
+ipcMain.on('clear-list', async function (e) {
+    dataSource = [];
+    refreshList();
+});
+
+ipcMain.on('refresh-list', async function (e) {
+    refreshList();
+});
---
+++ site.js
@@ -0,0 +1,41 @@
+const { ipcRenderer } = require('electron');
+const os = require("os");
+window.$ = window.jQuery = require('jquery');
+
+ipcRenderer.send('refresh-list');
+
+ipcRenderer.on('refresh-list', function (e, data) {
+    let table = $('table#list');
+    let tbody = $('<tbody></tbody>')
+    let i = 0;
+    data.forEach(element => {
+        let tr = $('<tr title="' + encodeHtml(element.value) + '"></tr>');
+        //tr.append('<td>#' + (i + 1) + '</td>');
+        tr.append('<td class="text-muted">' + formatDateIsoString(element.date) + '</td>');
+        tr.append('<td>' + encodeHtml(element.value) + '</td>');
+        tr.append('<td class="text-end">' + element.value.length + '</td>');
+        tr.append('<td class="text-center">' + createRowButtons(i) + '</td>')
+        tbody.append(tr);
+        i++;
+    });
+    table.html(tbody);
+});
+
+function createRowButtons(i) {
+    let html = '<div class="btn-group">';
+    html += '<button class="btn btn-sm btn-outline-dark" onclick="copyItem(' + i + ')"><i class="fa-solid fa-clipboard"></i></button>';
+    html += '<button type="button" class="btn btn-sm btn-outline-danger" onclick="deleteItem(' + i + ')"><i class="fa-solid fa-trash"></i></button>';
+    html += '</div>';
+    return html;
+}
+
+$('#btn-clear-list').on('click', function () { ipcRenderer.send('clear-list'); });
+$('#btn-clear-duplicates').on('click', function () { ipcRenderer.send('clear-duplicates'); });
+
+function copyItem(i) { ipcRenderer.send('copy-item', i); }
+function deleteItem(i) { ipcRenderer.send('delete-item', i); }
+
+function encodeHtml(str) { return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;'); }
+
+function formatDateIsoString(str) { return new Date(str).toTimeString().split(' ')[0]; }
+
